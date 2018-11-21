c442a7a41c01df557de2dfd2c6a5b208cb46efac
rocky <rocky-gow@hotmail.com>
2018-11-21 13:19:36 +0100

    index.html

---
+++ javascript/mijnjavascript.js
@@ -0,0 +1,16 @@
+var a = 10;
+if(a > 9) {
+    console.log("Hallo is groter dan 9 hoor");
+}
+for (var i = 0; i < 10; i++) {
+    console.log("i heeft nu de waarde " + i);
+}
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "blauw"
+
+}
+console.log(mijnauto.kleur);
+
