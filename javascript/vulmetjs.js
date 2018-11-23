292893e101030cb24e1efee2a7147ee60b2ba129
rocky <rocky-gow@hotmail.com>
2018-11-23 13:49:30 +0100

    javascriptopdracht3

--- javascript/vulmetjs.js
+++ javascript/vulmetjs.js
@@ -3,3 +3,4 @@ document.getElementById("subkop").innerText ="mijn geweldige subkop";
 document.getElementById("aap").src="img/aap.jpg";
 document.getElementById("aap").src="img/animal-ape-fur-395712.jpg";
 document.getElementById("aap").src="img/animal-3744602_640.jpg";
+
